8b3c3e7bd72dda9663dd8fbabe72fd7c83f76083
George Qiao <george.qiao@wisau.com.au>
2017-10-26 23:40:53 +1100

    added  check sys.py

---
+++ samples/features/machine-learning-services/python/getting-started/check sys.py
@@ -0,0 +1,9 @@
+import sys
+import os
+print("*******************************")
+print(sys.version)
+print(sys.platform)
+print(os.path)
+print(os.getcwd())
+# print(os.environ)
+print("*******************************")
